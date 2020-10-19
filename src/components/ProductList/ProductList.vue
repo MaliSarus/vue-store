7d4e9b6631e9a4199497b3b78945db5ba0806e1e
MaliSarus <43217693+malisarus@users.noreply.github.com>
2020-10-20 01:36:33 +0300

    add ProductList

---
+++ src/components/ProductList/ProductList.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul class="catalog__list">
+        <ProductItem v-for="(product,index) in products"
+                     :title="product.title"
+                     :price="product.price"
+                     :image="product.image"
+                     :key="index"
+        />
+    </ul>
+</template>
+
+<script>
+    import ProductItem from "./ProductItem/ProductItem";
+
+    export default {
+        name: "ProductList",
+        components: {ProductItem},
+        props: ['products']
+    }
+</script>
+
+<style scoped>
+
+</style>
